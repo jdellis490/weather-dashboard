22a4f92cd80ae7331d4839456b4e926914b59877
Justin Ellis <jdellis490@gmail.com>
2022-04-24 20:12:30 -0700

    Displays Temp, Wind Speed, and Humidity

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,16 @@ function searchSubmit(event) {
     event.preventDefault();
     var inputCity = searchForm.value;
     console.log(inputCity);
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + APIKey;
+    if (!inputCity) {
+        alert("Please enter a city!");
+        return;
+    }
+    onSearch(inputCity);
+
+}
+
+function onSearch(searchValue) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial" + "&appid=" + APIKey;
     fetch(queryURL)
         .then(function(response) {
             console.log(response);
@@ -16,13 +25,27 @@ function searchSubmit(event) {
         })
         .then(function(data){
             console.log(data);
+            displayCurrentCity(data);
         })
         .catch(function(error){
             console.log(error);
         })
+}
 
-    }
-       
+function displayCurrentCity(response){
+    document.getElementById('city-display').innerHTML =`
+    <div>
+    <h3>${response.name}</h3>
+    <ul class= "list-unstyled">
+        <li>Temp: ${response.main.temp}</li>
+        <li>Humidity: ${response.main.humidity}%</li>
+        <li>Wind Speed: ${response.wind.speed} mph</li>
+    </ul>
+    </div>`;
+}
+        
+    
+    
      
 
 
